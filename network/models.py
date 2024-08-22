f026c204a305729ee854de1d5eba00999394750d
pgatundu <117097252+pgatundu@users.noreply.github.com>
2024-08-22 16:28:21 +0300

    second commit

--- network/models.py
+++ network/models.py
@@ -12,3 +12,10 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Posted {self.id} by {self.user} on {self.date.strftime('%I:%M %p %b %d %Y')}"
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_who_is_following")
+    user_follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_who_is_being_followed")
+
+    def __str__(self):
+        return f"{self.user} is following {self.user_follower}" 
